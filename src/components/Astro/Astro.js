070a32365a5c7fb3d4d158e74f9f52d307011ff6
Resaki <christian.reski@t-online.de>
2020-02-25 22:15:45 +0100

    aligning multiple images center

--- src/components/Astro/Astro.js
+++ src/components/Astro/Astro.js
@@ -1,9 +1,20 @@
 import React from "react";
 
+function importAll(r) {
+    return r.keys().map(r);
+  }
+  
+const images = importAll(require.context('../../images/astro', false, /\.(jpe?g)$/));
+
 class Astro extends React.Component {
     render() {
+        console.log(images)
         return (
-            <h1>Astro</h1>
+            <>
+                {images.map((image, index) => (
+                    <img src={image} key={"astro"+index}></img>
+                ))}
+            </>
         )
     }
 }
