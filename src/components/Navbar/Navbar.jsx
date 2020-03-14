dc015f924d29c76d87f704027aabe9728467d7b0
Christian Reski <rech1033@HS-Karlsruhe.de>
2020-03-14 12:08:34 +0100

    added responsive sidebar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,42 +12,83 @@ import Timelapse from "../Timelapse/Timelapse";
 import Shop from "../Shop/Shop";
 import Gear from "../Gear/Gear";
 import About from "../About/About";
-import "./Navbar.css"
+import "./Navbar.css";
+import Sidebar from "react-sidebar";
 
-export default function Navbar() {
-    return (
-        <Router>
-            <div className="layout">
-                <div className="navbar">
-                    <h2>Resaki Fotografie</h2>
-                    <nav>
-                        <ul>
-                            <li>
-                                <NavLink exact to="/" activeClassName="selected">Home</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/astro" activeClassName="selected">Astrofotografie</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/landscape" activeClassName="selected">Landschaftsfotografie</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/timelapse" activeClassName="selected">Zeitraffer</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/shop" activeClassName="selected">Shop</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/gear" activeClassName="selected">Ausrüstung</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/about" activeClassName="selected">About</NavLink>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-    
-                <div className="content">
+const mql = window.matchMedia(`(min-width: 800px)`);
+
+export default class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          sidebarDocked: mql.matches,
+          sidebarOpen: false
+        };
+     
+        this.mediaQueryChanged = this.mediaQueryChanged.bind(this);
+        this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+      }
+     
+      componentWillMount() {
+        mql.addListener(this.mediaQueryChanged);
+      }
+     
+      componentWillUnmount() {
+        this.state.mql.removeListener(this.mediaQueryChanged);
+      }
+     
+      onSetSidebarOpen(open) {
+        this.setState({ sidebarOpen: open });
+      }
+     
+      mediaQueryChanged() {
+        this.setState({ sidebarDocked: mql.matches, sidebarOpen: false });
+      }
+
+      sidebarLinks = 
+        <div className="navbar">
+        <h2>Resaki Fotografie</h2>
+        <nav>
+            <ul>
+                <li>
+                    <NavLink exact to="/" activeClassName="selected">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/astro" activeClassName="selected">Astrofotografie</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/landscape" activeClassName="selected">Landschaftsfotografie</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/timelapse" activeClassName="selected">Zeitraffer</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/shop" activeClassName="selected">Shop</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/gear" activeClassName="selected">Ausrüstung</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about" activeClassName="selected">About</NavLink>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
+
+      render() {
+        return (
+            <Router>
+                <div className="layout">
+                <Sidebar
+                    sidebar={this.sidebarLinks}
+                    sidebarClassName="navbar"
+                    open={this.state.sidebarOpen}
+                    docked={this.state.sidebarDocked}
+                    onSetOpen={this.onSetSidebarOpen}
+                    shadow={false}
+                >
+                    <div className="content">
                     <Switch>
                         <Route path="/about">
                             <About />
@@ -72,7 +113,9 @@ export default function Navbar() {
                         </Route>
                     </Switch>
                 </div>
-            </div>
-        </Router>
-    );
+                </Sidebar>
+                </div>
+            </Router>
+        );
+      }
 }
