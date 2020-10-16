ec3cc420442710b8aecafc28a3eae37296faa8a3
Resaki <christian.reski@t-online.de>
2020-10-16 18:27:54 +0200

    added logo to homescreen

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import logo from "../../ressources/logo512.png";
 import "./Header.css";
 
 export default function Header(props) {
   return (
     <div className="wrapper" id={props.id}>
-      <h1>{props.title}</h1>
+      <h1
+        style={
+          props.logo
+            ? {
+                float: "right",
+                marginRight: "5vmin"
+              }
+            : undefined
+        }
+      >
+        {props.logo ? (
+          <img src={logo} className="logo" alt="logo" />
+        ) : (
+          undefined
+        )}
+        {props.title}
+      </h1>
     </div>
   );
 }
--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -6,7 +6,7 @@ class Home extends React.Component {
   render() {
     return (
       <section id="home">
-        <Header title="resaki" />
+        <Header title="resaki" logo={true} />
         <img
           className="homeImage"
           src="https://firebasestorage.googleapis.com/v0/b/resaki-de.appspot.com/o/Images%2Finfrared%2FZastler%20Valley.jpg?alt=media&token=5580becf-8937-43b7-873a-108f88c28dd2"
