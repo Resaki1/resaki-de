2fb8b9c5e9fe3b5ec735bc87dcf5da4fb97882a8
Resaki <christian.reski@t-online.de>
2020-10-16 16:00:20 +0200

    removed legacy files

--- src/components/Pages/Landscape/Landscape.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ImageContent from "../../ImageContent/ImageContent";
-
-function Landscape() {
-  return <ImageContent name="Landscape" title="Landschaft" />;
-}
-
-export default Landscape;
